42710fbd2f5d5ebe5ca6152e5432be3086669d75
Senpai <danila.karpov@gmail.com>
2019-11-28 22:02:39 +0200

    Fix bootstrap link

--- react-taskit/src/App.js
+++ react-taskit/src/App.js
@@ -6,12 +6,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
+      
     </div>
   );
 }
