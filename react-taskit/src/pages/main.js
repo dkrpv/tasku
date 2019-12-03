e9200b71548e4ce33d9acc5013e2de4f502dfa9a
Senpai <danila.karpov@gmail.com>
2019-12-03 12:50:20 +0200

    Add animations

--- react-taskit/src/pages/main.js
+++ react-taskit/src/pages/main.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { setPage } from 'react';
+import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button } from 'react-bootstrap';
 import placeholder from './placeholder.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styled, { keyframes } from 'styled-components';
+import { tada } from 'react-animations';
 
-const Main = ({ setPage }) => (
+const PopUpTada = styled.div`animation: 1s ${keyframes`${tada}`}`;
+
+export default class Main extends Component {
+  render() {
+    return(
   <div className="Main">
   <div
       style={{
@@ -12,31 +19,19 @@ const Main = ({ setPage }) => (
         transform: 'translate(-50%, -50%)'
       }}
     >
+  <PopUpTada>
   <tbody>
     <table class="cardTable">
       <th>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={ placeholder } />
-        <Card.Body>
-          <Card.Title>Creating Tasks</Card.Title>
-          <Card.Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-          </Card.Text>
-          <Button variant="success" onClick={() => setPage('createTask')}>Create Task</Button>
-        </Card.Body>
-        </Card>
-        </th>
-        <th>
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={ placeholder } />
           <Card.Body>
-            <Card.Title>Doing Tasks</Card.Title>
+            <Card.Title>Creating Tasks</Card.Title>
             <Card.Text>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
             </Card.Text>
-            <Button variant="success" onClick={() => setPage('doTask')}>Look for Tasks</Button>
+            <Button variant="success" onClick={() => setPage('createTask')}>Create Task</Button>
           </Card.Body>
           </Card>
           </th>
@@ -44,19 +39,33 @@ const Main = ({ setPage }) => (
           <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={ placeholder } />
             <Card.Body>
-              <Card.Title>Adjust Preferences</Card.Title>
+              <Card.Title>Doing Tasks</Card.Title>
               <Card.Text>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
               </Card.Text>
-              <Button variant="success" onClick={() => setPage('profile')}>Profile</Button>
+              <Button variant="success" onClick={() => setPage('doTask')}>Look for Tasks</Button>
             </Card.Body>
             </Card>
             </th>
-      </table>
-    </tbody>
-  </div>
-  </div>
-)
-
-export default Main
+            <th>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={ placeholder } />
+              <Card.Body>
+                <Card.Title>Adjust Preferences</Card.Title>
+                <Card.Text>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </Card.Text>
+                <Button variant="success" onClick={() => setPage('profile')}>Profile</Button>
+              </Card.Body>
+              </Card>
+              </th>
+        </table>
+      </tbody>
+      </PopUpTada>
+    </div>
+    </div>
+    )
+  }
+}
--- react-taskit/src/pages/myTask.js
+++ react-taskit/src/pages/myTask.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { setPage } from 'react';
+import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert';
+import styled, { keyframes } from 'styled-components';
+import { fadeIn } from 'react-animations';
 
-const myTask = ({ setPage }) => (
-  <div className="profile">
-  <div
-      style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-      }}
-    >
-    <Alert variant="dark"
-      style={{
-        height: '60vh',
-        width: '70vh',
-      }}
-    >
-      <Alert.Heading>Your Tasks</Alert.Heading>
-      <p>
-        You have no tasks. <Alert.Link onClick={() => setPage('createTask')}>Create</Alert.Link> one now!
-      </p>
-      <hr />
-      <p className="mb-0">
-        Before creating tasks, please read our <Alert.Link>Terms And Conditions</Alert.Link>.
-      </p>
-  </Alert>
-  </div>
-  </div>
-)
+const FadeIn = styled.div`animation: 2s ${keyframes`${fadeIn}`}`;
 
-export default myTask
+export default class doTask extends Component {
+  render() {
+    return(
+    <div className="profile">
+    <div
+        style={{
+          position: 'absolute', left: '50%', top: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+      >
+      <FadeIn>
+      <Alert variant="dark"
+        style={{
+          height: '60vh',
+          width: '70vh',
+        }}
+      >
+        <Alert.Heading>Your Tasks</Alert.Heading>
+        <p>
+          You have no Tasks. <Alert.Link onClick={() => setPage('createTask')}>Create</Alert.Link> one now!
+        </p>
+        <hr />
+        <p className="mb-0">
+          Before creating tasks, please read our <Alert.Link>Terms And Conditions</Alert.Link>.
+        </p>
+    </Alert>
+    </FadeIn>
+    </div>
+    </div>
+  )
+  }
+}
