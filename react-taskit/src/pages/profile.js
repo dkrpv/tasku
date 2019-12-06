cdc41aeb55dd36db4edeb1e8346dedac771f0c43
Danila Karpov <danila.karpov@gmail.com>
2019-12-06 15:28:08 +0200

    Add profile animation

--- react-taskit/src/pages/profile.js
+++ react-taskit/src/pages/profile.js
@@ -5,11 +5,16 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import DoTask from './doTask';
 import MyTask from './myTask';
+import styled, { keyframes } from 'styled-components';
+import { fadeIn } from 'react-animations';
 
 var repCount = 0;
+var name = "Name Last";
+const FadeIn = styled.div`animation: 2s ${keyframes`${fadeIn}`}`;
 
 const profile = ({ setPage }) => (
   <div className="profile">
+  <FadeIn>
   <div
       style={{
         position: 'absolute', left: '50%', top: '50%',
@@ -21,7 +26,7 @@ const profile = ({ setPage }) => (
         width: '60vw'
       }}
   >
-      <h1>Name Lastname</h1>
+      <h1>{ name }</h1>
       <p>
         Reputation: { repCount }
       </p>
@@ -37,6 +42,7 @@ const profile = ({ setPage }) => (
       </Tabs>
   </Jumbotron>
   </div>
+  </FadeIn>
   </div>
 )
 
