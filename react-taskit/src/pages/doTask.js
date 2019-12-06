1aeedef55e0b5b4a6fd974f8072729f01cd07363
Danila Karpov <danila.karpov@gmail.com>
2019-12-06 15:24:33 +0200

    Add profile

--- react-taskit/src/pages/doTask.js
+++ react-taskit/src/pages/doTask.js
@@ -14,7 +14,7 @@ const DoTask = ({ setPage }) => {
           position: 'absolute', left: '50%', top: '50%',
           transform: 'translate(-50%, -50%)'
         }}
-      >
+      > 
       <FadeIn>
       <Alert variant="dark"
         style={{
--- react-taskit/src/pages/profile.js
+++ react-taskit/src/pages/profile.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import DoTask from './doTask';
+import MyTask from './myTask';
 
 var repCount = 0;
 
@@ -8,9 +13,29 @@ const profile = ({ setPage }) => (
   <div
       style={{
         position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
+        transform: 'translate(-50%, -50%)',
       }}
     >
+  <Jumbotron
+      style={{
+        width: '60vw'
+      }}
+  >
+      <h1>Name Lastname</h1>
+      <p>
+        Reputation: { repCount }
+      </p>
+      <p>
+      </p>
+      <Tabs defaultActiveKey="createTask" id="profileTabs">
+        <Tab eventKey="createTask" title="Create Task">
+          <DoTask />
+        </Tab>
+        <Tab eventKey="myTask" title="My Tasks">
+          <MyTask />
+        </Tab>
+      </Tabs>
+  </Jumbotron>
   </div>
   </div>
 )
