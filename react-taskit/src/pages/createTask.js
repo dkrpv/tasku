f7852aa5dd5588aba909768db9e9bdc9f00d2cc1
EetuPe <eetu.petanen@gmail.com>
2019-12-19 21:31:52 +0200

    Working database

--- react-taskit/src/pages/createTask.js
+++ react-taskit/src/pages/createTask.js
@@ -6,10 +6,54 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import styled, { keyframes } from 'styled-components';
 import { fadeIn } from 'react-animations';
+import 'firebase/firestore';
+import firebaseConfig from './firebaseConfig';
+import * as firebase from 'firebase/app';
+
 
 const FadeIn = styled.div`animation: 2s ${keyframes`${fadeIn}`}`;
 
-const createTask = ({ setPage }) => (
+class createTask extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+     address: "",
+     category: "",
+     city: "",
+     offer: "",
+     title: ""
+    };
+  }
+
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  addTask = e => {
+    e.preventDefault();
+    const db = firebase.firestore();
+    db.settings({
+        timestampsInSnapshots: true
+    });
+    const taskRef = db.collection("tasks").add({
+        address: this.state.address,
+        category: this.state.category,
+        city: this.state.city,
+        offer: this.state.offer,
+        title: this.state.title,
+    });  
+    this.setState({
+      address: "",
+      category: "",
+      city: "",
+      offer: "",
+      title: ""
+    });
+  };
+  render() {
+    return (
   <div className="createTask">
   <FadeIn>
   <div
@@ -21,48 +65,51 @@ const createTask = ({ setPage }) => (
     <Form>
       <Form.Group controlId="formGridTitle">
         <Form.Label className="formText">Title</Form.Label>
-        <Form.Control type="text" placeholder="Enter Title" />
+        <Form.Control type="text" name="title" placeholder="Enter Title" onChange={this.updateInput} value={this.state.title}/>
       </Form.Group>
 
       <Form.Row>
         <Form.Group as={Col} controlId="formOffer">
           <Form.Label className="formText">Offer</Form.Label>
           <InputGroup.Prepend>
-            <Form.Control placeholder="5.00" />
+            <Form.Control name="offer" placeholder="5.00" onChange={this.updateInput} value={this.state.offer}/>
               <InputGroup.Text id="inputGroupPrepend">€</InputGroup.Text>
             </InputGroup.Prepend>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formCategorye">
+        <Form.Group as={Col} controlId="formCategory">
           <Form.Label className="formText">Category</Form.Label>
-          <Form.Control as="select">
+          <Form.Control name="category" as="select" onChange={this.updateInput} value={this.state.category}>
             <option>...</option>
-            <option>...</option>
+            <option>Danila</option>
+            <option>Karpov</option>
           </Form.Control>
         </Form.Group>
 
         <Form.Group as={Col} controlId="formCity">
           <Form.Label className="formText">City</Form.Label>
-          <Form.Control placeholder="New York" />
+          <Form.Control type="text" name="city" placeholder="New York" onChange={this.updateInput} value={this.state.city}/>
         </Form.Group>
       </Form.Row>
 
     <Form.Group controlId="formAdress">
       <Form.Label className="formText">Address</Form.Label>
-      <Form.Control placeholder="20 W 34th St" />
+      <Form.Control type="text" name="address" placeholder="20 W 34th St" onChange={this.updateInput} value={this.state.address}/>
     </Form.Group>
 
     <Form.Group id="formTermBox">
       <Form.Check type="checkbox" className="formText" label="I have read and accept the terms and conditions" />
     </Form.Group>
 
-    <Button className="gradBut" type="submit">
+    <Button className="gradBut" type="submit" onClick={this.addTask}>
       <b>Create</b>
     </Button>
     </Form>
   </div>
   </FadeIn>
   </div>
-)
+  );
+}
+}
 
 export default createTask;
